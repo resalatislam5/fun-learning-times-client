5d54bd48b9f46cae01278885695e6a1f0d77c709
Resalat <resalatislam5@gmail.com>
2022-10-26 01:53:15 +0600

    header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { routes } from './routes/Routes';
 
+
 function App() {
   return (
     <div >
--- src/pages/sheared/Header/Header.js
+++ src/pages/sheared/Header/Header.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, {useState} from "react";
+import { Link } from 'react-router-dom';
+import  header_logo  from '../../../ass/header_logo.png'
+import DarkModeToggle from "react-dark-mode-toggle";
+import userProfile from '../../../ass/user.jpg'
 
 const Header = () => {
+    const [isDarkMode, setIsDarkMode] = useState(() => false);
     return (
         <div>
-            <h1>Header</h1>
+            <div className="navbar bg-base-100 py-4 shadow-lg mt-2 px-5 rounded-lg">
+                <div className="navbar-start mr-3">
+                    <div className="dropdown mr-2">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to='/courses'>Courses</Link></li>
+                        <li>
+                        <Link to='/faq' className="justify-between">FAQ</Link>
+                        </li>
+                        <li><Link to='/blog'>Blog</Link></li>
+                        <DarkModeToggle
+                        onChange={setIsDarkMode}
+                        checked={isDarkMode}
+                        size={80}
+                        />
+                    </ul>
+                    </div>
+                    <Link to='/'><img className='w-20 rounded-full ' src={header_logo} alt="logo" /></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex font-bold">
+                    <ul className="menu menu-horizontal p-0">
+                    <li><Link to='/courses'>Courses</Link></li>
+                    <li >
+                        <Link to='/faq'>FAQ</Link>
+                    </li>
+                    <li><Link to='/blog'>Blog</Link></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <Link to='/login' className="btn btn-error text-white  mr-2">Login</Link>
+                    <Link to='/signup' className="btn btn-error text-white lg:mr-5 ">sign up</Link>
+                    {/* {
+                        user && user?.uid ?
+                        <>
+                        <Link to='/login' className="btn btn-error text-white  mr-2">Login</Link>
+                        <Link to='/signup' className="btn btn-error text-white lg:mr-5 ">sign up</Link>
+                        </>:
+                        <>
+                            <div className="w-10 rounded-full mr-5">
+                            {
+                                user && user.displayName ?
+                                // <img src={displayName} alt="Profile" />
+                                :
+                                <img src={userProfile} alt="Profile" />
+
+                            }
+                            </div>                            
+                            <button className="btn btn-error text-white lg:mr-5 ">sign Out</button>
+                        </>
+                    } */}
+                    <DarkModeToggle className="hidden lg:visible"
+                        onChange={setIsDarkMode}
+                        checked={isDarkMode}
+                        size={80}
+                        />
+                </div>
+            </div>
         </div>
     );
 };
